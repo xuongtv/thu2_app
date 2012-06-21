31c405a2c05cae4a9cf272e0bc17062577917147
Tran Xuong <tranxuong.ict016@gmail.com>
2012-06-21 10:52:20 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thu2App::Application.config.secret_token = 'ff276966300730757de097384b513d373fe97fbac2b1ad561ac2c1075708aa85ac5f03b41f261741120618079f02dcd7d859a0326e4a1812c6baadfa48376f1f'
